682bd24607948740a76f2e63bb95460564c61a2e
Asif Khan <asifkhan.github@gmail.com>
2018-09-17 15:59:40 +0600

    option menu event created

--- app/src/main/java/com/example/asifkhan/customgridviewgallery/adapters/GalleryAdapter.java
+++ app/src/main/java/com/example/asifkhan/customgridviewgallery/adapters/GalleryAdapter.java
@@ -2,18 +2,21 @@ package com.example.asifkhan.customgridviewgallery.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.asifkhan.customgridviewgallery.R;
 import com.example.asifkhan.customgridviewgallery.models.Users;
 
 import java.util.ArrayList;
 
-public class GalleryAdapter extends BaseAdapter {
+public class GalleryAdapter extends BaseAdapter implements View.OnClickListener{
     private ArrayList<Users> users;
     private Context context;
 
@@ -53,6 +56,33 @@ public class GalleryAdapter extends BaseAdapter {
         photo.setImageResource(user.getPhoto());
         name.setText(user.getName());
         profession.setText(user.getProfession());
+        option.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        showOptionMenu(view);
+    }
+
+    private void showOptionMenu(View view) {
+        PopupMenu popupMenu=new PopupMenu(context,view);
+        popupMenu.getMenuInflater().inflate(R.menu.option_menu,popupMenu.getMenu());
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.edit:
+                        Toast.makeText(context, "Edit", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.remove:
+                        Toast.makeText(context, "Remove", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 }
